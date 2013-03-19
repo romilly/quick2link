07b588d9b4cb11c25b322e0f696cd94154c1b041
Romilly Cocking <romilly.cocking@gmail.com>
2013-03-19 08:41:49 +0000

    started to define microcontroller class

---
+++ microcontroller/microcontroller.h
@@ -0,0 +1,21 @@
+/*
+
+This class represents an Arduino or clone.
+
+We will eventually define an interface so we can use a mock for testing,
+and this class for deployment.
+
+Romilly Cocking 19 march 2013
+*/
+
+#ifndef Microcontroller_h
+#define Microcontroller_h
+
+class Microcontroller {
+    public:
+        Microcontroller();
+        ~Microcontroller();
+        void digitalWrite(int pin, value)
+}
+
+#endif
