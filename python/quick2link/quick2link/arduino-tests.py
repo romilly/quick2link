99d0e524fef378da0efdafc29e20780006481f43
sf105 <github@m3p.co.uk>
2013-03-30 21:29:28 +0000

    corrected corrected tests

--- python/quick2link/quick2link/arduino-tests.py
+++ python/quick2link/quick2link/arduino-tests.py
@@ -31,17 +31,6 @@ class ArduinoTest(unittest.TestCase):
         self.assertIn("digitalPin=7", self.arduino.ask(on_pin(7), whois()))
         self.assertIn("digitalPin=7", self.arduino.ask(whois()))
 
-# This test fails, but I don't understand it. Steve, over to you. I've commented it out for now.
-# Romilly
-    
-    # def testRepeatsInstructions(self):
-    #     self.assertEqual("111>?12d1o11d2{ip}ip}ip}p",
-    #         self.arduino.ask( echo(),
-    #             on_pin(12), digital_write(HIGH),
-    #             on_pin(11),
-    #             repeat(2, digital_read(), print_value()),
-    #             print_value()))
-
     def testEchoesProcessedCharacters(self):
         self.assertEqual("arduino x=0, digitalPin=13>?h", self.arduino.ask(echo(), whois()))
 
@@ -52,9 +41,18 @@ class ArduinoTest(unittest.TestCase):
         self.assertEqual("16>?16mp", self.arduino.ask(echo(), delay_millis(16), print_value()))
         self.assertEqual("75>?75up", self.arduino.ask(echo(), delay_micros(75), print_value()))
 
+    def testRepeatsInstructions(self):
+        # connect digital pins 11 <-> 12
+        self.arduino.ask(on_pin(12), digital_write(HIGH))
+        self.assertEqual("111>?11d2{ip}ip}ip}p",
+             self.arduino.ask( echo(),
+                 on_pin(11),
+                 repeat(2, digital_read(), print_value()),
+                 print_value()))
+
     def testWritesAndReadsDigitalPin(self):
         # connect digital pins 11 <-> 12
-        self.assertEqual("0>?12d1o11dip",
+        self.assertEqual("1>?12d1o11dip",
             self.arduino.ask(
                 echo(),
                 on_pin(12), digital_write(HIGH),
