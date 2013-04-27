f2dfd491e70ac56e889fe703ecbca45fbed34738
Steve Freeman <steve.freeman@quick2wire.com>
2013-04-27 20:14:22 +0000

    clean up

--- python/src/quick2link/arduinoi2c.py
+++ python/src/quick2link/arduinoi2c.py
@@ -2,19 +2,25 @@ from quick2wire.i2c import I2CMaster, writing, reading
 
 arduino_address = 0x04
 buffer_size = 64
+encoding='utf-8'
+
+def contents_of(reply):
+    reply_length = reply[0]
+    return reply[1:reply_length + 1].decode(encoding)
+    
 
 class ArduinoI2cTransport:
     def __init__(self, address=arduino_address):
         self.master = I2CMaster()
         self.address = address
 
-    def ask(self, text):
-        reply = self.master.transaction(
-            writing(self.address, text.encode('utf-8')),
-            reading(self.address, buffer_size))
-        reply_length = reply[0][0]
-        return reply[0][1:reply_length + 1].decode('utf-8')
+    def _request(self, text):
+        return self.master.transaction(
+            writing(self.address, text.encode(encoding)),
+            reading(self.address, buffer_size))[0]
         
+    def ask(self, text):
+        return contents_of(self._request(text))
 
     def close(self):
         self.master.close()
